0fabd739a6a1d86b8fddaa0e92af60a7fb8dcfaf
suhyunbaik <suhyunbaik89@gmail.com>
2019-07-13 20:57:42 +0900

    Refactor: 겹치는 코드 수정

--- short/api.py
+++ short/api.py
@@ -14,13 +14,14 @@ def main():
     form = ShortyForm(request.form)
     if request.method == 'POST' and form.validate():
         original_url = request.form['url']
-        name = request.form.get('name')
-        converted = sum([ord(_) for _ in original_url])
-        short_url = encode(converted)
-        if not name:
-            new_urls = URLS(original_url=original_url, short_url=short_url)
-            db.session.add(new_urls)
-            db.session.commit()
-            return render_template('main.html', form=form, msg=short_url)
+        short_url = request.form.get('name')
+        if not short_url:
+            converted = sum([ord(_) for _ in original_url])
+            short_url = encode(converted)
+
+        new_urls = URLS(original_url=original_url, short_url=short_url)
+        db.session.add(new_urls)
+        db.session.commit()
+        return render_template('main.html', form=form, msg=short_url)
 
     return render_template('main.html', form=form)
