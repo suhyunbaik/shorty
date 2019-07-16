10a72955736f6cbe2517b8714634c4d40e77a177
suhyunbaik <suhyunbaik89@gmail.com>
2019-07-17 00:10:37 +0900

    Refactor: 주석제거

--- short/tests/conftest.py
+++ short/tests/conftest.py
@@ -22,20 +22,12 @@ def client(app):
 
 @pytest.fixture(scope='session')
 def init_db():
-    # engine = create_engine(config_by_name['test'].SQLALCHEMY_DATABASE_URI, echo=True)
-    # Base.metadata.create_all(engine)
-    # Engine.create()
     meta = Base.metadata
     meta.bind = Engine
     meta.create_all()
     yield Engine
     meta.drop_all()
     Engine.dispose()
-
-    # db.init_app(app)
-    # db.create_all()
-    # yield db
-    # db.drop_all()
 
 
 @pytest.fixture(scope='session')
@@ -44,17 +36,3 @@ def session(init_db):
     yield Session
     Session.remove()
 
-
-# @pytest.fixture(scope='session')
-# def db():
-#     engine = create_engine(config_by_name['test'].SQLALCHEMY_DATABASE_URI, echo=True)
-#     session = sessionmaker(bind=engine)
-#     _db = {
-#         'engine': engine,
-#         'session': session
-#     }
-#     Base.metadata.create_all()
-#     yield _db
-#     Base.metadata.drop_all()
-#     engine.dispose()
-#
